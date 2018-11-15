41f1246f62e1b926115591b7eba47c5fd9a128f6
todyTech <y0unn@hanmail.net>
2018-11-15 14:49:38 +0900

    기본 CURD 기능 게시판: 목록 - Controller

--- springPrj/src/main/java/com/tody/board/controller/BoardController.java
+++ springPrj/src/main/java/com/tody/board/controller/BoardController.java
@@ -1,29 +1,36 @@
 package com.tody.board.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tody.board.service.BoardService;
+import com.tody.common.common.CommandMap;
 
 @Controller
 public class BoardController {
-
-	@RequestMapping(value="/board/boardList")
-	public String boardList() throws Exception {
-		return "/board/boardList";
-	}
 	
-	@RequestMapping(value="/board/boardWrite")
-	public String boardWrite() throws Exception {
-		return "/board/boardWrite";
-	}
+	Logger log = Logger.getLogger(getClass());
 	
-	@RequestMapping(value="/board/boardDetail")
-	public String boardDetail() throws Exception {
-		return "/board/boardDetail";
-	}
-	
-	@RequestMapping(value="/board/boardModify")
-	public String boardModify() throws Exception {
-		return "/board/boardModify";
-	}
+    @Resource(name="boardService")
+    private BoardService boardServcie;
+    
+    @RequestMapping(value="/board/boardList")
+    public ModelAndView openBoardList(CommandMap commandMap) throws Exception {
+        
+        ModelAndView mav = new ModelAndView("/board/boardList");
+        
+        List<Map<String,Object>> list = boardServcie.selectBoardList(commandMap);
+        mav.addObject("list", list);
+        
+        return mav;
+        
+    }
 	
 }
