2d597f1ede0b6cb76ab2ee1b3579de4edebb8fe6
todyDev <y0unn@hanmail.net>
2018-12-14 16:56:36 +0900

    Controller에 에러와 비밀번호 확인 로직만 추가

--- springPrj/src/main/java/com/tody/user/controller/JoinController.java
+++ springPrj/src/main/java/com/tody/user/controller/JoinController.java
@@ -1,6 +1,9 @@
 package com.tody.user.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +24,23 @@ public class JoinController {
 		ModelAndView mv = new ModelAndView("/join/signup");
 		mv.addObject("registerRequest", new RegisterRequest());
 		return mv;
+	}
+	
+	@RequestMapping(value="/signup", method=RequestMethod.POST)
+	public String joinSignupPOST(@Valid RegisterRequest regReq, Errors errors) throws Exception {
+		
+		if(errors.hasErrors()) {
+			
+			//비밀번호 확인
+			boolean chk = regReq.isPwEqualToCheckPw();
+			if(chk) {
+				errors.reject("checkPassword.notMatch");
+			}
+			
+			return "/join/signup";
+		}
+
+		return "redirect:/";
 	}
 
 }
