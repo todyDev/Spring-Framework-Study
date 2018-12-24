7ff071f6e176cda53ff2ab57921764391a995e6d
todyDev <y0unn@hanmail.net>
2018-12-24 16:38:47 +0900

    validation 플러그인 테스트를 위해 컨트롤러 수정

--- springPrj/src/main/java/com/tody/user/controller/JoinController.java
+++ springPrj/src/main/java/com/tody/user/controller/JoinController.java
@@ -1,22 +1,20 @@
 package com.tody.user.controller;
 
 import javax.annotation.Resource;
-import javax.validation.Valid;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.tody.user.domain.RegisterRequest;
-import com.tody.user.exception.AlreadyExistingEmailException;
-import com.tody.user.exception.AlreadyExistingIdException;
+import com.tody.common.common.CommandMap;
 import com.tody.user.service.UserService;
 
 @Controller
 @RequestMapping(value="/join")
 public class JoinController {
+	
+	Logger log = Logger.getLogger(getClass());
 	
 	@Resource(name="userService")
 	private UserService userSer;
@@ -32,36 +30,15 @@ public class JoinController {
 	}
 	
 	@RequestMapping(value="/signup")
-	public ModelAndView joinSignupGET() throws Exception {
-		ModelAndView mv = new ModelAndView("/join/signup");
-		mv.addObject("registerRequest", new RegisterRequest());
-		return mv;
+	public String joinSignupGET() throws Exception {
+		return "/join/signup";
 	}
 	
 	@RequestMapping(value="/signup", method=RequestMethod.POST)
-	public String joinSignupPOST(@Valid RegisterRequest regReq, Errors errors) throws Exception {
+	public String joinSignupPOST(CommandMap commandMap) throws Exception {
+
+		log.debug(commandMap.getMap());
 		
-		if(errors.hasErrors()) {
-			
-			//비밀번호 확인
-			boolean chk = regReq.isPwEqualToCheckPw();
-			if(chk) {
-				errors.reject("checkPassword.notMatch");
-			}
-			
-			return "/join/signup";
-		}
-
-		try {
-			userSer.register(regReq);
-		} catch (AlreadyExistingIdException e ) {
-			errors.rejectValue("ID", "duplicate.id");
-			return "/join/signup";
-		} catch (AlreadyExistingEmailException e ) {
-			errors.rejectValue("EMAIL", "duplicate.email");
-			return "/join/signup";
-		}
-
 		return "redirect:/";
 	}
 
