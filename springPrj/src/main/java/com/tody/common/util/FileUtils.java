cff16ba850405be2f3053f7c72a8978c7f874751
todyDev <y0unn@hanmail.net>
2019-01-10 16:14:23 +0900

    다중 업로드 처리 - FileUtils

--- springPrj/src/main/java/com/tody/common/util/FileUtils.java
+++ springPrj/src/main/java/com/tody/common/util/FileUtils.java
@@ -20,42 +20,46 @@ public class FileUtils {
 	@Resource(name="uploadPath")
 	String uploadPath;
 
-	public List<Map<String, Object>> parseFileInfo(Map<String, Object> map, MultipartFile file) throws Exception {
-		
-		log.debug("================== file start ==================");
-		log.debug("파일 이름: "+file.getName());
-		log.debug("파일 실제 이름: "+file.getOriginalFilename());
-		log.debug("파일 크기: "+file.getSize());
-		log.debug("content type: "+file.getContentType());
-		log.debug("파일 존재 유무: "+file.isEmpty());
-		log.debug("파일 데이터: file.getBytes()");
-		log.debug("inputStream: file.getInputStream()");
-		log.debug("transferTO: 업로드한 파일 데이터를 지정한 폴더에 저장");
-		log.debug("================== file   END ==================");
-		
-		String orgFileName = file.getOriginalFilename();
-		String orgFileExtension = orgFileName.substring(orgFileName.lastIndexOf("."));
-		String saveFileName = CommonUtils.getRandomString() + orgFileExtension;
+	public List<Map<String, Object>> parseFileInfo(Map<String, Object> map, MultipartFile[] file) throws Exception {
 		
 		String boardIDX = String.valueOf(map.get("IDX"));
 		String creaID = String.valueOf(map.get("CREA_ID"));
-
+		
 		File target = new File(uploadPath);
 		if(!target.exists()) target.mkdirs();
 		
-		target = new File(uploadPath, saveFileName);
-		file.transferTo(target);
-		
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-		Map<String, Object> listMap = new HashMap<String, Object>();
+		Map<String, Object> listMap = null;
 		
-		listMap.put("BOARD_IDX", boardIDX);
-		listMap.put("ORG_FILE_NAME", orgFileName);
-		listMap.put("SAVE_FILE_NAME", saveFileName);
-		listMap.put("FILE_SIZE", file.getSize());
-		listMap.put("CREA_ID", creaID);
-		list.add(listMap);
-
+		for(int i=0; i<file.length; i++ ) {
+			log.debug("================== file start ==================");
+			log.debug("파일 이름: "+file[i].getName());
+			log.debug("파일 실제 이름: "+file[i].getOriginalFilename());
+			log.debug("파일 크기: "+file[i].getSize());
+			log.debug("content type: "+file[i].getContentType());
+			log.debug("파일 존재 유무: "+file[i].isEmpty());
+			log.debug("파일 데이터: file.getBytes()");
+			log.debug("inputStream: file.getInputStream()");
+			log.debug("transferTO: 업로드한 파일 데이터를 지정한 폴더에 저장");
+			log.debug("================== file   END ==================");
+			
+			String orgFileName = file[i].getOriginalFilename();
+			String orgFileExtension = orgFileName.substring(orgFileName.lastIndexOf("."));
+			String saveFileName = CommonUtils.getRandomString() + orgFileExtension;
+			
+			target = new File(uploadPath, saveFileName);
+			file[i].transferTo(target);
+			
+			listMap = new HashMap<String, Object>();
+			
+			listMap.put("BOARD_IDX", boardIDX);
+			listMap.put("ORG_FILE_NAME", orgFileName);
+			listMap.put("SAVE_FILE_NAME", saveFileName);
+			listMap.put("FILE_SIZE", file[i].getSize());
+			listMap.put("CREA_ID", creaID);
+			list.add(listMap);
+		}
+		
 		return list;
 		
 	}
