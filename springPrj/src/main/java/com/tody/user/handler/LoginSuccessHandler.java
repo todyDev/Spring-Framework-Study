438c44eff40db1a6b4a9ec8725c9dc409409b767
todyDev <y0unn@hanmail.net>
2019-01-22 11:03:43 +0900

    로그인 성공시 이동할 URL을 결정하는 작업

--- springPrj/src/main/java/com/tody/user/handler/LoginSuccessHandler.java
+++ springPrj/src/main/java/com/tody/user/handler/LoginSuccessHandler.java
@@ -8,18 +8,28 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 	
 	private String defaultUrl;
+	
+	private RequestCache requestCache = new HttpSessionRequestCache();
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 		
 		clearAuthenticationException(request);
+		
+		sendRedirectStrategy(request, response, authentication);
 
 	}
 	
@@ -27,6 +37,20 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 		HttpSession session = request.getSession(false);
 		if(session==null) return;
 		session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+	}
+	
+	protected void sendRedirectStrategy(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+		SavedRequest savedRequest = requestCache.getRequest(request, response);
+		
+		if(savedRequest!=null) {
+			String targetUrl = savedRequest.getRedirectUrl();
+			redirectStrategy.sendRedirect(request, response, targetUrl);
+		} else {
+			redirectStrategy.sendRedirect(request, response, defaultUrl);
+		}
+		
 	}
 
 	public String getDefaultUrl() {
