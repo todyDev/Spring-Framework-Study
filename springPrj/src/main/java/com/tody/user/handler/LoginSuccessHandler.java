7c6c69830ca1a202fb9a017a81840623a58f5c01
todyDev <y0unn@hanmail.net>
2019-01-22 10:59:34 +0900

    로그인 실패 에러 세션 지우는 작업

--- springPrj/src/main/java/com/tody/user/handler/LoginSuccessHandler.java
+++ springPrj/src/main/java/com/tody/user/handler/LoginSuccessHandler.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
@@ -16,8 +18,15 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		
+		clearAuthenticationException(request);
 
+	}
+	
+	protected void clearAuthenticationException(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if(session==null) return;
+		session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
 	}
 
 	public String getDefaultUrl() {
