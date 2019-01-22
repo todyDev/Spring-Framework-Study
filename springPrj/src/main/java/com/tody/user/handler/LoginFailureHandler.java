c4774ccb6f1bfc21477df87a082a90f9d36a7985
todyDev <y0unn@hanmail.net>
2019-01-22 10:37:00 +0900

    로그인 실패 핸들러 생성

---
+++ springPrj/src/main/java/com/tody/user/handler/LoginFailureHandler.java
@@ -0,0 +1,58 @@
+package com.tody.user.handler;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+public class LoginFailureHandler implements AuthenticationFailureHandler {
+	
+	private String usernamename;
+	private String passwordname;
+	private String errormsgname;
+	private String defaultFailureUrl;
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getUsernamename() {
+		return usernamename;
+	}
+
+	public void setUsernamename(String usernamename) {
+		this.usernamename = usernamename;
+	}
+
+	public String getPasswordname() {
+		return passwordname;
+	}
+
+	public void setPasswordname(String passwordname) {
+		this.passwordname = passwordname;
+	}
+
+	public String getErrormsgname() {
+		return errormsgname;
+	}
+
+	public void setErrormsgname(String errormsgname) {
+		this.errormsgname = errormsgname;
+	}
+
+	public String getDefaultFailureUrl() {
+		return defaultFailureUrl;
+	}
+
+	public void setDefaultFailureUrl(String defaultFailureUrl) {
+		this.defaultFailureUrl = defaultFailureUrl;
+	}
+
+}
