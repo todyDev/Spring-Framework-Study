194e1fbfbaa238d0039d850b47a4b79ece4f4ce8
todyDev <y0unn@hanmail.net>
2019-01-22 16:46:36 +0900

    접근 불가 페이지에 회원 등급 표시 추가

--- springPrj/src/main/java/com/tody/user/handler/CustomAccessDeniedHandler.java
+++ springPrj/src/main/java/com/tody/user/handler/CustomAccessDeniedHandler.java
@@ -1,24 +1,40 @@
 package com.tody.user.handler;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
+import com.tody.user.domain.CustomUserDetails;
+
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
-	
-	Logger log = Logger.getLogger(getClass());
 	
 	private String errorpage;
 	
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Object principal = auth.getPrincipal();
+		
+		if(principal instanceof CustomUserDetails) {
+			String username = ((CustomUserDetails) principal).getUsername();
+			Collection<? extends GrantedAuthority> authenti = ((CustomUserDetails) principal).getAuthorities();
+			
+			request.setAttribute("username", username);
+			request.setAttribute("auth", authenti);
+		}
+		
+		request.getRequestDispatcher(errorpage).forward(request, response);
 		
 	}
 
